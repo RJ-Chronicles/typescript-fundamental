53c74a088dd851b683b3bb11e2b417bf5bf13b6e
Jondhalekar <2106957@cognizant.com>
2022-11-29 18:52:06 +0530

    final commit message

--- CLASSES/InterfaceInTS.js
+++
@@ -1,8 +0,0 @@
-var printProgrammerDetails = function (programmer) {
-    return "Hi there! I am ".concat(programmer.name, " and i am ").concat(programmer.profile, ". I earn ").concat(programmer.salary, " month.");
-};
-console.log(printProgrammerDetails({
-    name: "Jack",
-    salary: 2000,
-    profile: "Web developer"
-}));
--- FUNCTION/function-essential.js
+++
@@ -1,7 +0,0 @@
-var doSomeWork = function (x, xx, xxx) {
-    if (typeof x === "number") {
-        return x;
-    }
-    return "something is still pending!";
-};
-console.log(doSomeWork("12", 55, 44));
